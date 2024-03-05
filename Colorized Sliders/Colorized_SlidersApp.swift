4c2d755b552c417748d998a1f55e45b954cbb6c0
NatashaOvdina <natashaovdina@yahoo.com>
2024-03-05 15:11:59 +0300

    Initial Commit

---
+++ Colorized Sliders/Colorized_SlidersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Colorized_SlidersApp.swift
+//  Colorized Sliders
+//
+//  Created by Natalia Ovdina on 05.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Colorized_SlidersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
